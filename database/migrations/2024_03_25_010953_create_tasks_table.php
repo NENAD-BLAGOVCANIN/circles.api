ce5d4cd7d2e6dc761ee7af847ddf778676ad7c0a
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-03-25 02:25:53 +0100

    models and migrations

---
+++ database/migrations/2024_03_25_010953_create_tasks_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject');
+            $table->text('description')->nullable();;
+            $table->foreignId('lead_id')->nullable();
+            $table->unsignedBigInteger('team_id');
+            $table->foreign('team_id')->references('id')->on('teams')->onDelete('cascade');            
+            $table->unsignedBigInteger('assignedTo');
+            $table->foreign('assignedTo')->references('id')->on('users')->onDelete('cascade');            
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
