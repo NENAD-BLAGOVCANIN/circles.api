7b7d2dca33b6ca4e6d1b8c650c7220f255d117eb
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-03-30 04:00:32 +0100

    dashboard stats

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,12 +19,18 @@ class DashboardController extends Controller
         $contactCount = Contact::where('team_id', '=', $team_id)->count();
         $leadCount = Lead::where('team_id', '=', $team_id)->count();
         $taskCount = Task::where('team_id', '=', $team_id)->count();
+        $todoTasksCount = Task::where('team_id', '=', $team_id)->where('status', '=', Task::STATUS_TODO)->count();
+        $inProgressTasksCount = Task::where('team_id', '=', $team_id)->where('status', '=', Task::STATUS_IN_PROGRESS)->count();
+        $doneTasksCount = Task::where('team_id', '=', $team_id)->where('status', '=', Task::STATUS_DONE)->count();
 
         $data = [
             "teamMembersCount" => $teamMemberCount,
             "contactCount" => $contactCount,
             "leadCount" => $leadCount,
             "taskCount" => $taskCount,
+            "todoTasksCount" => $todoTasksCount,
+            "inProgressTasksCount" => $inProgressTasksCount,
+            "doneTasksCount" => $doneTasksCount
         ];
 
         return response()->json($data);
